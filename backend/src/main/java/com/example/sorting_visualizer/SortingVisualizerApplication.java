2c283f66cabbe63250e88d64cbe6a7d440091dec
sujeetyadav021 <yadavsujeet21@gmail.com>
2024-09-18 13:55:56 +0530

    added springboot directory

---
+++ backend/src/main/java/com/example/sorting_visualizer/SortingVisualizerApplication.java
@@ -0,0 +1,13 @@
+package com.example.sorting_visualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortingVisualizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortingVisualizerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/sorting_visualizer/SortingVisualizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sorting_visualizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortingVisualizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
